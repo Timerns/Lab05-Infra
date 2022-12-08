d2668c37f52a96ec5f72388960fc4de88eea6c3c
Killassa <gregory.reymermet@gmail.com>
2022-12-08 11:53:45 +0100

    Create static page

---
+++ Static HTTP server/src/assets/js/main.js
@@ -0,0 +1,33 @@
+var elDiceOne       = document.getElementById('dice1');
+var elDiceTwo       = document.getElementById('dice2');
+var elDiceThree     = document.getElementById('dice3');
+
+setInterval(() => {
+  rollDice(Math.floor((Math.random() * 6) + 1), Math.floor((Math.random() * 6) + 1), Math.floor((Math.random() * 6) + 1));
+}, 3000);
+
+
+function rollDice(diceOne, diceTwo, diceThree) { 
+  console.log(diceOne + ' ' + diceTwo + ' ' + diceThree);
+
+  for (var i = 1; i <= 6; i++) {
+    elDiceOne.classList.remove('show-' + i);
+    if (diceOne === i) {
+      elDiceOne.classList.add('show-' + i);
+    }
+  }
+
+  for (var k = 1; k <= 6; k++) {
+    elDiceTwo.classList.remove('show-' + k);
+    if (diceTwo === k) {
+      elDiceTwo.classList.add('show-' + k);
+    }
+  }
+
+  for (var k = 1; k <= 6; k++) {
+    elDiceThree.classList.remove('show-' + k);
+    if (diceTwo === k) {
+      elDiceThree.classList.add('show-' + k);
+    }
+  }
+}
