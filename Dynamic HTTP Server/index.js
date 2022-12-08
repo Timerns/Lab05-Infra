d87f31c17c4f752df388b99e3ca8f245d044daed
eric.peronett <eric.peronetti@heig-vd.ch>
2022-12-08 17:05:50 +0100

    Returning 3 dice values on /

--- Dynamic HTTP Server/index.js
+++ Dynamic HTTP Server/index.js
@@ -5,14 +5,31 @@ var app = express();
 
 app.get('/', (req, res) => {
     res.send(JSON.stringify({
-        dices: {
-            1: 7,
-            2: 6,
-            3: 3,
-        }
+        dice: [
+            getRandomDiceValue(),
+            getRandomDiceValue(),
+            getRandomDiceValue()
+        ]
     }));
 });
 
 app.listen(PORT, () => {
     console.log(`Listening on port ${PORT} !`);
 });
+
+/**
+ * Returns a random dice value [1, 9]
+ * @returns {number} The dice value
+ */
+function getRandomDiceValue() {
+    return getRandomInt(6) + 1;
+}
+
+/**
+ * Returns a random value in the range [0, max[
+ * @param {number} max The max number
+ * @returns A number in the range [0, max[
+ */
+function getRandomInt(max) {
+    return Math.floor(Math.random() * max);
+}
