5f43c83222c88c89c890899f8afecb6b7765af22
eric.peronett <eric.peronetti@heig-vd.ch>
2022-12-12 13:24:33 +0100

    Start / Stop working

--- Management UI/index.js
+++ Management UI/index.js
@@ -1,11 +1,72 @@
+const path = require('path')
+
 const express = require('express');
 const app = express();
 
 var Docker = require('dockerode');
 var docker = new Docker();
 
+app.set('view engine', 'ejs');
+
 app.get('/', async function (req, res) {
-  res.send(await docker.info());
+    var containers = [];
+    for (var containerBase of await docker.listContainers({all: true})) {
+        var container = {};
+
+        //Name
+        var finalName = "";
+        for (var name of containerBase.Names) {
+            if (name.startsWith('/')) {
+                name = name.substring(1);
+            }
+            finalName += name + " ";
+        }
+        finalName = finalName.substring(0, finalName.length - 1);
+
+        //Ports
+        var ports = [];
+        for (var port of containerBase.Ports) {
+            console.log(port);
+            ports.push(`${port.PrivatePort}:${port.PublicPort} (${port.Type})`);
+        }
+
+        //Object container
+        container.id = containerBase.Id;
+        container.name = finalName;
+        container.status = containerBase.Status;
+        container.state = containerBase.State;
+        container.ports = ports;
+
+        //Push to containers
+        containers.push(container);
+    }
+    console.log(containers);
+
+    res.render('pages/index', { 
+        containers: containers
+    });
 });
 
-app.listen(80);
+app.get('/start/:id', async (req, res) => {
+    try {
+        await docker.getContainer(req.params.id).start();
+        res.sendStatus(200);
+    } catch (e) {
+        console.log(e.reason);
+        res.status(500).send(e.reason);
+    }
+});
+
+app.get('/stop/:id', async (req, res) => {
+    try {
+        await docker.getContainer(req.params.id).stop();
+        res.sendStatus(200);
+    } catch (e) {
+        console.log(e.reason);
+        res.status(500).send(e.reason);
+    }
+});
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.listen(3000);
---
+++ Management UI/public/js/index.js
@@ -0,0 +1,12 @@
+function startContainer(id) {
+    fetch('/start/' + id)
+        .then(async data => {
+            $('#errorModal .modal-body p').text(await data.text());
+            $('#errorModal').modal('show');
+        });
+}
+
+function stopContainer(id) {
+    fetch('/stop/' + id)
+        .then(data => console.log(data));
+}
