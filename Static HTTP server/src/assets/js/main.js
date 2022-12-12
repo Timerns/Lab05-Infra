3b91652e328c221f8ad8662ec42fc1deb8e94a32
Killassa <gregory.reymermet@gmail.com>
2022-12-12 09:53:08 +0100

    Fix display

--- Static HTTP server/src/assets/js/main.js
+++ Static HTTP server/src/assets/js/main.js
@@ -2,10 +2,14 @@ var elDiceOne       = document.getElementById('dice1');
 var elDiceTwo       = document.getElementById('dice2');
 var elDiceThree     = document.getElementById('dice3');
 
-setInterval(() => {
-  rollDice(Math.floor((Math.random() * 6) + 1), Math.floor((Math.random() * 6) + 1), Math.floor((Math.random() * 6) + 1));
-}, 3000);
+setInterval(loadRandomDiceValue, 3000);
 
+function loadRandomDiceValue() {
+  $.getJSON("http://localhost:80/", function(values) {
+    console.log(values);
+    rollDice(values.dice[0], values.dice[1], values.dice[2])
+  });
+}
 
 function rollDice(diceOne, diceTwo, diceThree) { 
   console.log(diceOne + ' ' + diceTwo + ' ' + diceThree);
@@ -17,16 +21,16 @@ function rollDice(diceOne, diceTwo, diceThree) {
     }
   }
 
-  for (var k = 1; k <= 6; k++) {
-    elDiceTwo.classList.remove('show-' + k);
-    if (diceTwo === k) {
-      elDiceTwo.classList.add('show-' + k);
+  for (var j = 1; j <= 6; j++) {
+    elDiceTwo.classList.remove('show-' + j);
+    if (diceTwo === j) {
+      elDiceTwo.classList.add('show-' + j);
     }
   }
 
   for (var k = 1; k <= 6; k++) {
     elDiceThree.classList.remove('show-' + k);
-    if (diceTwo === k) {
+    if (diceThree === k) {
       elDiceThree.classList.add('show-' + k);
     }
   }
