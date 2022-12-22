b8931e8f9ef52253316f6cb22629b208986d8759
eric.peronett <eric.peronetti@heig-vd.ch>
2022-12-22 12:00:46 +0100

    Fixed the images add not working + navbar added + separation

--- Management UI/index.js
+++ Management UI/index.js
@@ -35,93 +35,85 @@ app.get('/', async function (req, res) {
         container.status = containerBase.Status;
         container.state = containerBase.State;
         container.ports = ports;
-        // console.log(containerBase);
 
         //Push to containers
         containers.push(container);
     }
-    // console.log(containers);
 
     res.render('pages/index', { 
         containers: containers
     });
 });
 
-
 app.get('/images', async function (req, res) {
-  var images = [];
-  for (var imageBase of await docker.listImages({all: true})) {
-      var image = {};
+    var images = [];
+    for (var imageBase of await docker.listImages({all: true})) {
+        var image = {};
 
-      // //Name
-      // var finalName = "";
-      // for (var name of containerBase.Names) {
-      //     if (name.startsWith('/')) {
-      //         name = name.substring(1);
-      //     }
-      //     finalName += name + " ";
-      // }
-      // finalName = finalName.substring(0, finalName.length - 1);
+        //Tags
+        var tags = [];
+        for (var tag of imageBase.RepoTags) {
+            tags.push(tag);
+        }
 
-       //containers
-       var containers = [];
-       for (var container of imageBase.RepoTags) {
-        containers.push(container);
-       }
+        //Object container
+        image.id = imageBase.Id;
+        image.size = parseFloat(imageBase.Size / 1024 / 1024).toFixed(2) + "MB";
+        image.createdAt = new Date(imageBase.Created * 1000).toISOString().substring(0, 10);
+        image.tags = tags;
 
+        //Push to containers
+        images.push(image);
+    }
 
-      //Tags
-      var tags = [];
-      for (var tag of imageBase.RepoTags) {
-        tags.push(tag);
-      }
-
-      //Object container
-      image.id = imageBase.Id;
-      image.size = parseFloat(imageBase.Size / 1024 / 1024).toFixed(2) + "MB";
-      image.createdAt = new Date(imageBase.Created * 1000).toISOString().substring(0, 10);
-      image.tags = tags;
-      console.log(imageBase);
-
-      //Push to containers
-      images.push(image);
-  }
-
-  res.render('pages/images', { 
-      images: images
-  });
+    res.render('pages/images', { 
+        images: images
+    });
 });
 
 app.get('/start/:id', async (req, res) => {
     try {
         await docker.getContainer(req.params.id).start();
-        res.sendStatus(200);
+        onSuccess(res);
     } catch (e) {
-        console.log(e.reason);
-        res.status(500).send(e.reason);
+        onError(res, e);
     }
 });
 
 app.get('/stop/:id', async (req, res) => {
     try {
         await docker.getContainer(req.params.id).stop();
-        res.sendStatus(200);
+        onSuccess(res);
     } catch (e) {
-        console.log(e.reason);
-        res.status(500).send(e.reason);
+        onError(res, e);
     }
 });
 
-app.get('/run/:id', async (req, res) => {
-  try {
-      await docker.run(req.params.id);
-      res.sendStatus(200);
-  } catch (e) {
-      console.log(e.reason);
-      res.status(500).send(e.reason);
-  }
+app.get('/add/:id', async (req, res) => {
+    try {
+        await docker.createContainer({ Image: req.params.id });
+        onSuccess(res);
+    } catch (e) {
+        onError(res, e);
+    }
 });
 
+app.get('/remove/:id', async (req, res) => {
+    try {
+        await docker.getContainer(req.params.id).remove();
+        onSuccess(res);
+    } catch (e) {
+        onError(res, e);
+    }
+});
+
+function onSuccess(res) {
+    res.status(200).send(undefined);
+}
+
+function onError(res, e) {
+    res.status(500).send(e.message);
+}
 
 app.use(express.static(path.join(__dirname, 'public')));
 
--- Management UI/public/js/index.js
+++ Management UI/public/js/index.js
@@ -1,17 +1,37 @@
-function startContainer(id) {
-    fetch('/start/' + id)
-        .then(async data => {
-            $('#errorModal .modal-body p').text(await data.text());
-            $('#errorModal').modal('show');
-        });
+function startContainer(id, button) {
+    executeRoute('/start/' + id, button);
 }
 
-function stopContainer(id) {
-    fetch('/stop/' + id)
-        .then(data => console.log(data));
+function stopContainer(id, button) {
+    executeRoute('/stop/' + id, button);
 }
 
-function runImage(id) {
-  fetch('/run/' + id)
-      .then(data => console.log(data));
+function addContainer(id, button) {
+    executeRoute('/add/' + id, button);
+}
+
+function removeContainer(id, button) {
+    executeRoute('/remove/' + id, button);
+}
+
+function executeRoute(route, button) {
+    var oldValue = button.innerHTML;
+    button.innerHTML = '<div class="spinner-border" style="width: 1rem; height: 1rem;" role="status"></div>';
+
+    fetch(route, button)
+        .then(handleResponse)
+        .catch(() => button.innerHTML = oldValue);
+}
+
+async function handleResponse(data) {
+    if (data.status == 200) {
+        document.location.reload();
+    }
+    showError(await data.text());
+    throw Error();
+}
+
+function showError(text) {
+    $('#errorModal .modal-body p').text(text);
+    $('#errorModal').modal('show');
 }
